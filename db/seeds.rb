4e8cee1455a497f2d7ce6e2f99299920b34425de
Shinya-works <shinya-works@gmail.com>
2021-12-09 13:33:21 +0000

    予備

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,11 @@ User.create!( name: "Sample User",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+              
+50.times do |n|
+  name = "タスク#{n+1}"
+  deteil = "タスク詳細"
+  Task.create!( name: name, 
+              deteil: deteil,
+              )
+end
