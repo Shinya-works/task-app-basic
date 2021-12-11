1a0c1148f868347c9bf7ff43e4c207564b4ee78e
Shinya-works <shinya-works@gmail.com>
2021-12-11 19:06:10 +0000

    タスクモデルのバリデーション設定完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
     def index
       @tasks = Task.order("id DESC")
+    end
+    
+    def show
     end
     
     def new
@@ -11,9 +14,10 @@ class TasksController < ApplicationController
     
     def create
       @task = Task.new(task_params)
+      @task.save
       if @task.save
         flash[:success] = "タスクの新規作成に成功しました。"
-        redirect_to tasks_path
+        redirect_to @task
       else
         render :new
       end
@@ -25,17 +29,19 @@ class TasksController < ApplicationController
     def update
       if @task.update_attributes(task_params)
         flash[:success] = "タスクの更新に成功しました。"
-        redirect_to tasks_path
+        redirect_to @task
       else
         render :edit
       end
     end
     
     def destroy
-      
+      @task.destroy
+      redirect_to tasks_url
     end
     
     private
+    
       def task_params
         params.permit(:name, :deteil)
       end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user, optional: true
+  validates :name, presence: true
+  validates :deteil, presence: true, length: { minimum: 5 }
 end
