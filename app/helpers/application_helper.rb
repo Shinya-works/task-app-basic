d17d40d94ce3b10b214b2a8c6892fb76a43f0d89
Shinya-works <shinya-works@gmail.com>
2021-12-13 03:52:47 +0000

    テキスト中の文字列URLをリンク化完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ require "uri"
   def text_url_to_link(text)
     URI.extract(text, ["http", "https"]).uniq.each do |url|
       sub_text = ""
-      sub_text << "<a href=" << " traget=\"_blank\">" << url << "</a>"
+      sub_text << "<a href=" << url << " traget=\"_blank\">" << url << "</a>"
       text.gsub!(url, sub_text)
     end
     
