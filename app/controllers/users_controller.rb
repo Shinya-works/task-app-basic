ab7fc277048db15256843155447ce5478c29682b
Shinya-works <shinya-works@gmail.com>
2021-12-13 14:31:49 +0000

    ユーザー更新ページ実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @users = User.all
@@ -22,10 +23,27 @@ class UsersController < ApplicationController
       render :new
     end
   end
+    
+  def edit
+  end
+    
+  def update
+    if @user.update_attributes(user_params)
+        flash[:success] = "ユーザー情報を更新しました。"
+        redirect_to @user
+    else
+      render :edit      
+    end
+  end
+
   
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end 
 end
